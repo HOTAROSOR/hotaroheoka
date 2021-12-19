bff0de49e6f876a06a089dbc1f8d7b203c32a929
HOTAROHEOKA <96379257+HOTAROSOR@users.noreply.github.com>
2021-12-19 16:30:23 +0300

    Add files via upload

---
+++ telesetup.py
@@ -0,0 +1,4 @@
+from telethon import TelegramClient, events
+from telethon.tl import types, functions
+
+client = TelegramClient('/home/lonami/AppData/Telethon/lonami.session', api_id, api_hash, update_workers=1).start()
